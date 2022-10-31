2a51a4783a182a8c7351f5b7b6958bb6aca43107
Joseph Lander <josephclander@gmail.com>
2022-10-31 12:34:18 +0000

    Add 02 building a route exercise sols

---
+++ hello_web_project/app.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: false
+
+# file: app.rb
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+# This allows the app code to refresh
+# without having to restart the server.
+class Application < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  # Declares a route that responds to a request with:
+  #  - a GET method
+  #  - the path /
+  get '/' do
+    # The code here is executed when a request is received,
+    # and need to send a response.
+
+    # We can simply return a string which
+    # will be used as the response content.
+    # Unless specified, the response status code
+    # will be 200 (OK).
+    return 'This is the home page.'
+  end
+
+  get '/hello' do
+    name = params[:name]
+    "Hello #{name}"
+    # url => http://localhost:9292/example?name=Joe
+    # response => Hello Joe
+  end
+
+  post '/submit' do
+    name = params[:name]
+    message = params[:message]
+    "Thanks #{name}, you sent this message: '#{message}'"
+  end
+end
---
+++ hello_web_project/config.ru
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+# file: config.ru
+
+require './app'
+run Application
