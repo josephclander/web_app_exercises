6377f08cac9b984bb8afa39ae734f949fa3d69ac
Joseph Lander <josephclander@gmail.com>
2022-10-31 15:13:40 +0000

    Add music library exercise sol

--- music_library_database_app/app.rb
+++ music_library_database_app/app.rb
@@ -13,4 +13,24 @@ class Application < Sinatra::Base
     also_reload 'lib/album_repository'
     also_reload 'lib/artist_repository'
   end
+
+  get '/albums' do
+    repo = AlbumRepository.new
+    result_set = repo.all
+
+    list = result_set.map do |album|
+      album.title
+    end
+    list.join(', ')
+  end
+
+  post '/albums' do
+    repo = AlbumRepository.new
+    album = Album.new
+    album.title = params[:title]
+    album.release_year = params[:release_year]
+    album.artist_id = params[:artist_id]
+
+    repo.create(album)
+  end
 end
--- music_library_database_app/spec/integration/application_spec.rb
+++ music_library_database_app/spec/integration/application_spec.rb
@@ -10,4 +10,22 @@ describe Application do
   # class so our tests work.
   let(:app) { Application.new }
 
+  context 'GET albums' do
+    it 'returns all albums' do
+      response = get('/albums')
+
+      expect(response.status).to eq 200
+      expect(response.body).to eq 'Surfer Rosa, Waterloo, Super Trouper, Bossanova, Lover, Folklore, I Put a Spell on You, Baltimore, Here Comes the Sun, Fodder on My Wings, Ring Ring'
+    end
+  end
+
+  context 'POST albums' do
+    it 'creates a new album' do
+      response = post('/albums', title: 'Voyage', release_year: '2022', artist_id: 2)
+      result_set = get('/albums')
+      expected_result = result_set.body.split(', ').last
+      expect(response.status).to eq 200
+      expect(expected_result).to eq 'Voyage'
+    end
+  end
 end
